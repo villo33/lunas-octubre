597d537ac3106f1654a0fe3be945e8c7648002ca
villo33 <romeroovidio02@yahoo.com>
2026-04-16 17:19:57 -0500

    fix: corregir variables de entorno de Cloudinary para Render y habilitar subida de imágenes

--- server.js
+++ server.js
@@ -18,11 +18,10 @@ app.use(express.static(path.join(__dirname, "public")));
 const API = "/api";
 
 /* ================== CLOUDINARY ================== */
-
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 /* ================== MULTER ================== */
